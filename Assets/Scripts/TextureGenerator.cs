dcdb22095bf282e54c8b3048a69db421e87c5fc3
Ayoub Khammassi <ayoub.khammassi1997@gmail.com>
2019-10-27 00:39:19 +0100

    Initial commit

---
+++ Assets/Scripts/TextureGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[ExecuteAlways]
+public class TextureGenerator : MonoBehaviour
+{
+    [Header("Texture Sheet")]
+    public Texture2D texSheet;
+    public int nRows;
+    public int nCols;
+
+    public Texture3D finalTexture; //final 3D texture interpolted from the 2D slices
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
