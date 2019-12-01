3bca36618f06fd13f098b378ec38909c7c4ffecb
Ayoub Khammassi <ayoub.khammassi1997@gmail.com>
2019-12-01 23:47:43 +0100

    Compute shader writes to Tex3D and slicers works

---
+++ Assets/Scripts/CubeMapGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class CubeMapGenerator : MonoBehaviour
+{
+    [Header("The used compute shader")]
+    public ComputeShader cmpShader;
+    public Shader slicer;
+    public Material mat;
+    public bool testing = false;
+    public RenderTexture finalTex;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateCubeMap();
+            mat = new Material(slicer);
+        }
+    }
+
+    public void CreateCubeMap()
+    {
+        RenderTextureDescriptor desc = new RenderTextureDescriptor();
+        desc.colorFormat = RenderTextureFormat.ARGB32;
+        desc.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+        desc.enableRandomWrite = true;
+        desc.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.B8G8R8A8_UNorm;
+        desc.msaaSamples = 1;
+        desc.height = Camera.main.pixelHeight;
+        desc.width = Camera.main.pixelWidth;
+        desc.volumeDepth = 512;
+
+        finalTex = new RenderTexture(desc);
+        finalTex.name = "test";
+        finalTex.Create();
+
+        if (finalTex.IsCreated())
+        {
+            cmpShader.SetTexture(0, "Result", finalTex);
+            cmpShader.Dispatch(0, desc.width,desc.height,512);
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (mat == null)
+        {
+            mat = new Material(slicer);
+        }
+
+        if(testing)
+        {
+            
+            mat.SetTexture("_VolumeTex", finalTex);
+            //mat.SetFloat("_Slice", 0.7f);
+            Graphics.Blit(source, destination, mat);
+        }
+    }
+}
---
+++ Assets/Shaders/Tex3DSlicer.shader
@@ -0,0 +1,56 @@
+﻿Shader "Hidden/Tex3DSlicer"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_Volumetex ("3D texture", 3D) = "white" {}
+		_Slice ("Slice", Range(0,1) ) = 1 
+		//TODO: add wich dimension to fix
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+			sampler3D _VolumeTex;
+			float _Slice;
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex3D(_VolumeTex, float3(i.uv, _Slice));
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
